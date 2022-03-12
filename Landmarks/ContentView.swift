68c2f75b45bda1b444381d181432ac179f52e557
cap811 <58876414+cap811@users.noreply.github.com>
2022-03-12 20:14:07 +0200

    Some credential changes

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -2,8 +2,6 @@
 //  ContentView.swift
 //  Landmarks
 //
-//  Created by Maily Eiubov on 3/12/22.
-//
 
 import SwiftUI
 
--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -2,8 +2,6 @@
 //  LandmarksApp.swift
 //  Landmarks
 //
-//  Created by Maily Eiubov on 3/12/22.
-//
 
 import SwiftUI
 
