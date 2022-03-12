8a792dd7e237741cb3a95239a747c660cfa89935
cap811 <58876414+cap811@users.noreply.github.com>
2022-03-12 20:10:56 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Maily Eiubov on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
